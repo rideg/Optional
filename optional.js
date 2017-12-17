8fc379090ed04dea9a6e4417f6ad2d2a8dc1dd10
rideg <sandor.rideg@gmail.com>
2017-12-16 21:07:23 -0800

    apply jshint suggestions

--- optional.js
+++ optional.js
@@ -19,132 +19,133 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-;(function() {
-  'use strict';
-  /**
-   * @param {?T|undefined} value
-   * @template T
-   * @constructor
-   * @return {!Optional<T>}
-   */
-  function Optional(value) {
-    /** @private @const {!T} */
-    this.value_;
-    if(value != null) {
-      this.value_ = value;
+/*global Promise, exports:true, module:true */
+(function () {
+
+    'use strict';
+    /**
+     * @param {?T|undefined} value
+     * @template T
+     * @constructor
+     * @return {!Optional<T>}
+     */
+    function Optional(value) {
+        if (value !== null && value !== undefined) {
+            /** @private @const {!T} */
+            this.value = value;
+        }
     }
-  };
 
-  Optional.prototype = Object.create(null);
+    Optional.prototype = Object.create(null);
 
-  /** @return {boolean} */
-  Optional.prototype.isPresent = function() {
-    return this.value_ != null;
-  };
+    /** @return {boolean} */
+    Optional.prototype.isPresent = function () {
+        return this.value !== undefined;
+    };
 
-  /** @return {boolean} */
-  Optional.prototype.isAbsent = function() {
-    return this.value_ == null;
-  };
+    /** @return {boolean} */
+    Optional.prototype.isAbsent = function () {
+        return !this.isPresent();
+    };
 
-  /** 
-   * @template T
-   * @method
-   * @return {!T} 
-   */
-  Optional.prototype.get = function() {
-    if(this.isPresent()) {
-      return this.value_;
-    }
-    throw new Error('Value is absent');
-  };
-
-  /** 
-   * @template T
-   * @param {!T} defaultValue
-   * @return {!T}
-   */
-  Optional.prototype.orElse = function(defaultValue) {
-    return this.value_ == null ? defaultValue : this.value_;
-  };
-
-  /**
-   * @template T
-   * @param {function(!T):void} callback
-   */
-  Optional.prototype.ifPresent = function(callback) {
-    if(this.value_ != null) {
-      callback(this.value_);
-    }
-  };
-
-  /** 
-   * @template T, V
-   * @param {function(!T): V} mapper
-   */
-  Optional.prototype.map = function(mapper) {
-    if(this.value_ == null) {
-      return ABSENT;
-    }
-    return OptionalHolder.of(mapper(this.value_));
-  };
-
-  // Optional promise interface
-  if(Promise) {
     /** 
      * @template T
-     * @return Promise<T>
+     * @method
+     * @return {!T} 
      */
-    Optional.prototype.promise = function() {
-      return new Promise(function(resolve, reject) {
-       if(this.value_ == null) {
-         reject();
-       } else {
-         resolve(this.get());
-       }
-      }.bind(this));
+    Optional.prototype.get = function () {
+        if (this.isPresent()) {
+            return this.value;
+        }
+        throw new Error('Value is absent');
     };
-  }
 
-  /** @return {string} */
-  Optional.prototype.toString = function() {
-    if(this.value_ == null) {
-      return 'Optional[absent]';
+    /** 
+     * @template T
+     * @param {!T} defaultValue
+     * @return {!T}
+     */
+    Optional.prototype.orElse = function (defaultValue) {
+        return this.isAbsent() ? defaultValue : this.value;
+    };
+
+    /**
+     * @template T
+     * @param {function(!T):void} callback
+     */
+    Optional.prototype.ifPresent = function (callback) {
+        if (this.isPresent()) {
+            callback(this.value);
+        }
+    };
+
+    var ABSENT = Object.seal(new Optional(null)),
+        OptionalHolder = Object.create(null);
+
+    /** 
+     * @template T, V
+     * @param {function(!T): V} mapper
+     */
+    Optional.prototype.map = function (mapper) {
+        if (this.isAbsent()) {
+            return ABSENT;
+        }
+        return Object.seal(new Optional(mapper(this.value)));
+    };
+
+    // Optional promise interface
+    if (Promise) {
+        /** 
+         * @template T
+         * @return Promise<T>
+         */
+        Optional.prototype.promise = function () {
+            return new Promise(function (resolve, reject) {
+                if (this.isAbsent()) {
+                    reject();
+                } else {
+                    resolve(this.value);
+                }
+            }.bind(this));
+        };
     }
-    return 'Optional[\''+this.value_+'\']';
-  };
 
-  var ABSENT = Object.seal(new Optional(null));
+    /** @return {string} */
+    Optional.prototype.toString = function () {
+        if (this.isAbsent()) {
+            return 'Optional[absent]';
+        }
+        return 'Optional[\'' + this.value + '\']';
+    };
 
-  var OptionalHolder = Object.create(null);
-  
-  /**
-   * @template T
-   * @param {?T|undefined} value
-   * @return !Optional<T>
-   */
-  OptionalHolder.of = function(value) {
-    return Object.seal(new Optional(value));
-  };
 
-  /** 
-   * @template T
-   * @return !Optional<T> 
-   */
-  OptionalHolder.absent = function() {
-    return ABSENT;
-  };
-  
-  Object.seal(OptionalHolder);
+    /**
+     * @template T
+     * @param {?T|undefined} value
+     * @return !Optional<T>
+     */
+    OptionalHolder.of = function (value) {
+        return Object.seal(new Optional(value));
+    };
 
-  if(typeof exports !== 'undefined') {
-    if(typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = OptionalHolder;
+    /** 
+     * @template T
+     * @return !Optional<T> 
+     */
+    OptionalHolder.absent = function () {
+        return ABSENT;
+    };
+
+    Object.seal(OptionalHolder);
+
+    if (exports === undefined) {
+        this.Optional = OptionalHolder;
     } else {
-     exports = OptionalHolder;
-   }
-  } else {
-    this['Optional'] = OptionalHolder;
-  }
-}).call(this);
+        if (module === undefined) {
+            exports = OptionalHolder;
+        } else if (module.exports) {
+            exports = module.exports = OptionalHolder;
+        }
+    }
+}(this));
 
